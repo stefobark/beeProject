e4cf1a39df10fd5429f52a5aa8ba9fda7eb9e3ac
blacklight <steven.j.barker.jr@gmail.com>
2016-02-18 10:35:09 -0800

    try again

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
 <script src="processing.min.js"></script>
+<canvas data-processing-sources="bees.pde Bee.pde Brain.pde DNA.pde EcoRules.pde Ecosystem.pde Graph.pde NeuralBees.pde Obstacle.pde Population.pde Target.pde"></canvas>
